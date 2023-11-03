04200ad87c7072d233965f2b1315026bdf6053c4
sametcn99 <sametcn99@gmail.com>
2023-11-03 05:14:03 +0300

    Add analytics component and package for tracking user behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import NavbarComponent from "./components/navbar";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <main className="min-h-screen">
             <NavbarComponent />
             {children}
+            <Analytics />
           </main>
         </Providers>
       </body>
