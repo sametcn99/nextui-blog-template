8d07e528459c4d60faae5fb46ca34674076f7246
sametcn99 <sametcn99@gmail.com>
2023-11-14 22:53:55 +0300

    some changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+module.exports = {
+    experimental: {
+        nextScriptWorkers: true,
+    },
+}
 module.exports = nextConfig;
