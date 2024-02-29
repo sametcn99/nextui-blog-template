24985c55d952688db058076b9a4037fb4dfda3a1
samet <sametcn99@gmail.com>
2024-02-29 23:52:57 +0300

    added all filter to date filters

--- components/sidebar/components/dateItems.tsx
+++ components/sidebar/components/dateItems.tsx
@@ -14,18 +14,30 @@ export default function DateItems({ dates, dateStart, dateEnd }: RootState) {
   const handleEndDateChange = (newEndDate: any) => {
     dispatch(setEndDate(newEndDate));
   };
-
-  return dates.map((date: any, index: number) => (
-    <li
-      key={index}
-      className="select-none hover:ml-1"
-      id={`${dateStart[index]} ${index}/${dateEnd[index]}`}
-      onClick={() => {
-        handleStartDateChange(dateStart[index]);
-        handleEndDateChange(dateEnd[index]);
-      }}
-    >
-      {dates[index]}
-    </li>
-  ));
+  return (
+    <>
+      <li
+        className="select-none hover:ml-1"
+        onClick={() => {
+          handleStartDateChange("");
+          handleEndDateChange("");
+        }}
+      >
+        All
+      </li>
+      {dates.map((date: any, index: number) => (
+        <li
+          key={index}
+          className="select-none hover:ml-1"
+          id={`${dateStart[index]} ${index}/${dateEnd[index]}`}
+          onClick={() => {
+            handleStartDateChange(dateStart[index]);
+            handleEndDateChange(dateEnd[index]);
+          }}
+        >
+          {dates[index]}
+        </li>
+      ))}
+    </>
+  );
 }
